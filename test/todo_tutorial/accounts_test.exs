9eda4f3cabd269958beb1ae9ece4e7deb3503c64
Akitaka Wakasugi <akitaka@AkitakanoMacBook-Pro.local>
2020-08-20 14:34:26 +0900

    fix accounts.ex/list_users/0 and make create_user/1, returns error changeset with invalid data

--- test/todo_tutorial/accounts_test.exs
+++ test/todo_tutorial/accounts_test.exs
@@ -1,21 +1,32 @@
 defmodule AccountsTest do
   use ExUnit.Case
 
-  describe "Accounts" do
-    @valid_attrs %{user_name: "John"}
+  @valid_attrs %{user_name: "John"}
+  @invalid_attrs %{user_name: nil}
 
-    def user_fixture(attrs \\ %{}) do
-      {:ok, task} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Todo.create_user()
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Map.merge(@valid_attrs)
+      |> Accounts.create_user()
 
-      task
-    end
+    user
+  end
 
-    test "list_users/0 returns the list of users" do
+  describe "list_users/0" do
+    test "returns the list of users", do
       user = user_fixture()
       assert Accounts.list_users() == [user]
     end
   end
+
+  describe "create_user/1"
+    test "creates a user with valid data" do
+      assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
+      assert user.user_name == @valid_attrs[user_name]
+    end
+
+    test "returns error changeset with invalid data" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+    end
 end
