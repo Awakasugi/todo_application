2d38a9bf72656edfc850db67d41cb831cd61b0df
Akitaka Wakasugi <akitaka@AkitakanoMacBook-Pro.local>
2020-08-26 16:22:42 +0900

    add edit function and update function on lib/todo_tutorial_web/controllers/task_controller.ex and make lib/todo_tutorial_web/templates/user/edit.html.eex

--- lib/todo_tutorial_web/controllers/task_controller.ex
+++ lib/todo_tutorial_web/controllers/task_controller.ex
@@ -93,4 +93,24 @@ defmodule TodoTutorialWeb.UserController do
     user = Accounts.get_user!(id)
     render(conn, "show.html", user: user)
   end
+
+  def edit(conn, %{"id" => id}) do
+    user = Accounts.get_user!(id)
+    changeset = Accounts.change_user(user)
+    render(conn, "edit.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Accounts.get_user!(id)
+
+    case Accounts.update_user(user, user_params) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "User updated successfully.")
+        |> redirect(to: Routes.user_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", user: user, changeset: changeset)
+    end
+  end
 end
