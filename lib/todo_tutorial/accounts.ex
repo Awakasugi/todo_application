e89666941ba179f69674bfed33e1df3c06f2bb1c
Akitaka Wakasugi <akitaka@AkitakanoMacBook-Pro.local>
2020-08-26 13:24:07 +0900

    make lib/todo_tutorial/accounts.ex test/todo_tutorial/accounts_test.ex change_user get_userupdate_user delete_user function

--- lib/todo_tutorial/accounts.ex
+++ lib/todo_tutorial/accounts.ex
@@ -18,4 +18,21 @@ defmodule TodoTutorial.Accounts do
     |> User.changeset(attrs)
     |> Repo.insert()
   end
+
+  def change_user(%User{} = user, attrs \\ %{}) do
+    User.changeset(user, attrs)
+  end
+
+  def get_user!(id), do: Repo.get!(User, id)
+
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
+
 end
--- test/todo_tutorial/accounts_test.exs
+++ test/todo_tutorial/accounts_test.exs
@@ -5,6 +5,7 @@ defmodule AccountsTest do
   alias TodoTutorial.Accounts.User
 
   @valid_attrs %{name: "John"}
+  @update_attrs %{name: "Chris"}
   @invalid_attrs %{name: nil}
 
   def user_fixture(attrs \\ %{}) do
@@ -31,6 +32,42 @@ defmodule AccountsTest do
 
     test "returns error changeset with invalid data" do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+    end
+  end
+
+  describe "change_user/1" do
+    test "returns a user changeset" do
+      user = user_fixture()
+      assert %Ecto.Changeset{} = Accounts.change_user(user)
+    end
+  end
+
+  describe "get_user!/1" do
+    test "returns the user with given id" do
+      user = user_fixture()
+      assert Accounts.get_user!(user.id) == user
+    end
+  end
+
+  describe "update_user/2" do
+    test "updates the user with valid data" do
+      user = user_fixture()
+      assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
+      assert user.name == "Chris"
+    end
+
+    test "returns error changeset with invalid data" do
+      user = user_fixture()
+      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
+      assert user == Accounts.get_user!(user.id)
+    end
+  end
+
+  describe "delete_user/1" do
+    test "deletes the user" do
+      user = user_fixture()
+      assert {:ok, %User{}} = Accounts.delete_user(user)
+      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
     end
   end
 end
