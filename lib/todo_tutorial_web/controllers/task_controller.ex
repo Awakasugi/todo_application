226ce52a32973b6205f08c5aab36c4fcd587ba15
Akitaka Wakasugi <akitaka@AkitakanoMacBook-Pro.local>
2020-08-26 15:34:23 +0900

    add user_controller module and lib/todo_tutorial_web/templates/user/ lib/todo_tutorial_web/views/user_view.ex

--- lib/todo_tutorial_web/controllers/task_controller.ex
+++ lib/todo_tutorial_web/controllers/task_controller.ex
@@ -60,3 +60,32 @@ defmodule TodoTutorialWeb.TaskController do
     |> redirect(to: Routes.task_path(conn, :index))
   end
 end
+
+defmodule TodoTutorialWeb.UserController do
+  use TodoTutorialWeb, :controller
+
+  alias TodoTutorial.Accounts
+  alias TodoTutorial.Accounts.User
+
+  def index(conn, _params) do
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
+  end
+
+  def new(conn, _params) do
+    changeset = Accounts.change_user(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> redirect(to: Routes.task_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+end
